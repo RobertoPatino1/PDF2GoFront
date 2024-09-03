e1adb0ea65c27c48bffe2fa8fea29d7fc936ee94
RobertoPatino1 <rapatino@espol.edu.ec>
2024-09-03 17:32:27 -0500

    Added navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
+<!-- <script>import Header from './components/Header.vue';</script> -->
+
 <template>
-  <div>Hello World!!!</div>
+  <v-app>
+    <v-main>
+      <Header></header>
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Header,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
