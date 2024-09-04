e6aedec1b6347f8ec1fb891d366963068dc68f91
RobertoPatino1 <rapatino@espol.edu.ec>
2024-09-03 23:03:36 -0500

    Added tab changes and panels

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
 	<v-app>
 		<v-main>
-			<Header></Header>
-			<InputField></InputField>
+			<Header :activeTab="tab" @update:activeTab="tab = $event" />
+			<LiveEditorPanel v-if="tab === 'Live Editor'" />
+			<!-- TODO: Add upload document panel -->
 		</v-main>
 	</v-app>
 </template>
 
 <script>
 	import Header from './components/shared/Header.vue';
-	import InputField from './components/liveEditor/InputField.vue';
+	import Panels from './components/liveEditor/LiveEditorPanel.vue';
+	import { ref } from 'vue';
+	import LiveEditorPanel from './components/liveEditor/LiveEditorPanel.vue';
 
 	export default {
 		name: 'App',
-
 		components: {
 			Header,
-			InputField,
+			Panels,
 		},
-
-		data: () => ({
-			//
-		}),
+		setup() {
+			const tab = ref('Live Editor'); // Valor inicial para la pestaña activa
+			return { tab };
+		},
 	};
 </script>
